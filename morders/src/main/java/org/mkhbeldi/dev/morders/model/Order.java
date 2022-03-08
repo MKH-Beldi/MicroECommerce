9f7ccaf59c0216db8189a663f15a82488da15f8d
MKH-BELDI <mohamadkhalil.beldi@esprit.tn>
2022-03-08 15:07:54 +0100

    Order microservice developed

---
+++ morders/src/main/java/org/mkhbeldi/dev/morders/model/Order.java
@@ -0,0 +1,26 @@
+package org.mkhbeldi.dev.morders.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer productId;
+
+    @Temporal(TemporalType.TIME)
+    private Date orderDate;
+
+    private Integer quantity;
+
+    private Boolean orderPaied;
+
+}
