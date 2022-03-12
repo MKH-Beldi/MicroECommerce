a7833ff9c8adf5abeefea253a008bbf29c87e5ef
MKH-BELDI <mohamadkhalil.beldi@esprit.tn>
2022-03-12 09:28:55 +0100

    Spring Cloud Config

--- mproducts/src/main/java/org/mkhbeldi/dev/mproducts/configuration/ApplicationPropertiesConfiguration.java
+++ mproducts/src/main/java/org/mkhbeldi/dev/mproducts/configuration/ApplicationPropertiesConfiguration.java
@@ -3,10 +3,12 @@ package org.mkhbeldi.dev.mproducts.configuration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("custom-configs")
+@RefreshScope
 @Getter
 @Setter
 public class ApplicationPropertiesConfiguration {
